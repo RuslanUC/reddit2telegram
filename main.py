fe2362ed637bf99faf0b710beb199a326395baef
RuslanUC <dev_ruslan_uc@protonmail.com>
2025-09-02 16:58:08 +0300

    add logging and infinite loop

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import platform
-from asyncio import new_event_loop
+from asyncio import new_event_loop, sleep
 from io import BytesIO
 from os import environ, urandom
 from time import time
-from typing import Generator
+from typing import Generator, TypeVar, ParamSpec, Callable
 from urllib.parse import urlparse
 
 from httpx import AsyncClient, Auth, Request, Response
+from loguru import logger
 from pyrogram import Client
 from pyrogram.enums import ParseMode
+from pyrogram.errors import FloodWait
 from pyrogram.types import InputMediaPhoto
 
 
@@ -22,6 +24,8 @@ class BearerAuth(Auth):
 
 
 class RedditPost:
+    __slots__ = ("id", "fullname", "subreddit", "title", "images")
+
     def __init__(self, id_: str, fullname: str, subreddit: str, title: str, images: list[str]) -> None:
         self.id = id_
         self.fullname = fullname
@@ -32,6 +36,10 @@ class RedditPost:
     @property
     def url(self) -> str:
         return f"https://www.reddit.com/r/{self.subreddit}/comments/{self.id}/"
+
+    def __repr__(self) -> str:
+        slots = ", ".join(f"{slot}={getattr(self, slot)!r}" for slot in self.__slots__)
+        return f"{self.__class__.__name__}({slots}, url={self.url!r})"
 
 
 class RedditClient:
@@ -50,12 +58,11 @@ class RedditClient:
         self._expires_at: int = 0
 
     def make_auth_url(self) -> str:
-        state = urandom(16).hex()
         return (
             f"https://www.reddit.com/api/v1/authorize"
             f"?client_id={self._client_id}"
             f"&response_type=code"
-            f"&state={state}"
+            f"&state={urandom(16).hex()}"
             f"&redirect_uri={self._redirect_uri}"
             f"&duration=permanent"
             f"&scope=history"
@@ -80,7 +87,14 @@ class RedditClient:
                 auth=(self._client_id, self._client_secret),
             )
 
+            logger.trace(f"Got reddit oauth code exchange response: status={resp.status_code}")
             if resp.status_code != 200:
+                try:
+                    body = resp.json()
+                except ValueError:
+                    body = "<not json>"
+                logger.debug(f"OAuth code exchange response body: {body}")
+
                 raise RuntimeError(f"Failed to get access token: error code {resp.status_code}")
 
             creds_resp = resp.json()
@@ -100,7 +114,14 @@ class RedditClient:
                     auth=(self._client_id, self._client_secret),
                 )
 
+                logger.trace(f"Got reddit token refresh response: status={resp.status_code}")
                 if resp.status_code != 200:
+                    try:
+                        body = resp.json()
+                    except ValueError:
+                        body = "<not json>"
+                    logger.debug(f"OAuth token refreshe response body: {body}")
+
                     raise RuntimeError(f"Failed to get access token: error code {resp.status_code}")
 
                 creds_resp = resp.json()
@@ -122,11 +143,20 @@ class RedditClient:
                 headers={"User-Agent": self._user_agent},
                 params=params,
             )
+            logger.trace(f"Got reddit /upvoted response: status={upvoted_resp.status_code}")
             if upvoted_resp.status_code != 200:
-                raise RuntimeError(f"Failed to get upvoted posts: error code {upvoted_resp.status_code}, body: {upvoted_resp.json()}")
+                try:
+                    body = upvoted_resp.json()
+                except ValueError:
+                    body = "<not json>"
+                logger.debug(f"OAuth token refreshe response body: {body}")
+
+                raise RuntimeError(f"Failed to get upvoted posts: error code {upvoted_resp.status_code}")
 
             upvoted_body = upvoted_resp.json()
             posts = upvoted_body["data"]["children"]
+
+            logger.debug(f"Got {len(posts)} before processing")
 
             for post in reversed(posts):
                 if post["kind"] != "t3":
@@ -138,19 +168,25 @@ class RedditClient:
                     if "gallery_data" not in post \
                             or not isinstance(post["gallery_data"], dict) \
                             or "items" not in post["gallery_data"] \
-                            or not isinstance(post["gallery_data"]["items"], list) \
-                            or "media_metadata" not in post \
+                            or not isinstance(post["gallery_data"]["items"], list):
+                        logger.info(f"Post {post['id']} has invalid \"gallery_data\" field: {post['gallery_data']}")
+                        continue
+
+                    if "media_metadata" not in post \
                             or not (isinstance(post["media_metadata"], dict)):
+                        logger.info(f"Post {post['id']} has invalid \"media_metadata\" field: {post['media_metadata']}")
                         continue
 
                     for item in post["gallery_data"]["items"]:
                         if "media_id" not in item:
+                            logger.info(f"Post {post['id']}: invalid item: {item}")
                             continue
 
                         metadata = post["media_metadata"][item["media_id"]]
                         if "s" not in metadata or not isinstance(metadata["s"], dict) \
                                 or "u" not in metadata["s"] or not isinstance(metadata["s"]["u"], str) \
                                 or not metadata["s"]["u"]:
+                            logger.info(f"Post {post['id']}: invalid metadata: {metadata}")
                             continue
 
                         images.append(metadata["s"]["u"])
@@ -165,11 +201,13 @@ class RedditClient:
                             or "url" not in post["preview"]["images"][0]["source"] \
                             or not isinstance(post["preview"]["images"][0]["source"]["url"], str) \
                             or not post["preview"]["images"][0]["source"]["url"]:
+                        logger.info(f"Post {post['id']} has invalid \"preview\" field: {post['preview']}")
                         continue
 
                     images.append(post["preview"]["images"][0]["source"]["url"])
 
                 if not images or len(images) > 10:
+                    logger.info(f"Post {post['id']} does not have any images or has more than 10 images")
                     continue
 
                 result.append(RedditPost(
@@ -180,11 +218,29 @@ class RedditClient:
                     images=images,
                 ))
 
+        logger.debug(f"Got {len(result)} after processing")
+
         return result
+
+
+T = TypeVar("T")
+P = ParamSpec("P")
+
+
+async def _flood_wait(func: Callable[P, T], *args, **kwargs) -> T | None:
+    attempts = 5
+    for i in range(attempts):
+        try:
+            return await func(*args, **kwargs)
+        except FloodWait as e:
+            logger.warning(f"Got FloodWait of {e.value} seconds, waiting...")
+            if i == (attempts - 1):
+                raise
+            await sleep(e.value + 1)
+
 
 async def main() -> None:
     reddit_username = environ["REDDIT_USERNAME"]
-    reddit_last_known_id = None
     reddit = RedditClient(
         client_id=environ["REDDIT_API_ID"],
         client_secret=environ["REDDIT_API_SECRET"],
@@ -194,6 +250,7 @@ async def main() -> None:
     )
 
     channel_id = int(environ["CHANNEL_ID"])
+    log_chat_id = int(environ["LOG_CHAT_ID"])
     bot = Client(
         name="reddit2telegram",
         api_id=int(environ["TG_API_ID"]),
@@ -211,50 +268,68 @@ async def main() -> None:
         print(f"Refresh token: {refresh_token}")
 
     async with bot:
-        posts = await reddit.get_upvoted(reddit_username, reddit_last_known_id)
-        for post in posts[-2:]:
-            print(f"Post {post.id}:")
-            print(f"  id={post.id!r}")
-            print(f"  fullname={post.fullname!r}")
-            print(f"  subreddit={post.subreddit!r}")
-            print(f"  title={post.title!r}")
-            print(f"  url={post.url!r}")
-            print(f"  images={post.images!r}")
+        reddit_last_known_id = environ.get("REDDIT_LAST_KNOWN_ID")
 
-            image_files: list[BytesIO] = []
-            for idx, image in enumerate(post.images):
-                photo = BytesIO()
-                async with AsyncClient() as cl:
-                    async with cl.stream("GET", image) as resp:
-                        async for chunk in resp.aiter_bytes(1024 * 64):
-                            photo.write(chunk)
+        while True:
+            try:
+                posts = await reddit.get_upvoted(reddit_username, reddit_last_known_id)
+            except Exception as e:
+                logger.opt(exception=e).error("Failed to get upvoted posts")
+                await bot.send_message(log_chat_id, "Failed to get upvoted posts, check logs for exact error")
+                await sleep(60 * 5)
+                continue
 
-                name = urlparse(image).path.split("/")[-1]
-                if not name:
-                    name = f"{post.fullname}_{idx}.jpg"
-                setattr(photo, "name", name)
-                image_files.append(photo)
+            for post in posts:
+                logger.debug(f"Post: {post!r}")
 
-            caption = f"{post.title}\n\n[Post link]({post.url})"
+                if not post.images or len(post.images) > 10:
+                    logger.info(f"Skipping post {post.id} ({post.title!r}): {len(post.images)=}")
+                    continue
 
-            if len(post.images) == 1:
-                await bot.send_photo(
-                    chat_id=channel_id,
-                    photo=image_files[0],
-                    caption=caption,
-                )
-            elif len(post.images) > 1:
-                media = [
-                    InputMediaPhoto(file, caption=caption if idx == 0 else "")
-                    for idx, file in enumerate(image_files)
-                ]
+                logger.info(f"Sending post {post.id} ({post.title!r})")
 
-                await bot.send_media_group(
-                    chat_id=channel_id,
-                    media=media,
-                )
+                image_files: list[BytesIO] = []
+                for idx, image in enumerate(post.images):
+                    photo = BytesIO()
+                    async with AsyncClient() as cl:
+                        async with cl.stream("GET", image) as resp:
+                            async for chunk in resp.aiter_bytes(1024 * 64):
+                                photo.write(chunk)
 
+                    name = urlparse(image).path.split("/")[-1]
+                    if not name:
+                        name = f"{post.fullname}_{idx}.jpg"
+                    setattr(photo, "name", name)
+                    image_files.append(photo)
 
+                caption = f"{post.title}\n\n[Post link]({post.url})"
+
+                if len(post.images) == 1:
+                    await _flood_wait(
+                        bot.send_photo,
+                        chat_id=channel_id,
+                        photo=image_files[0],
+                        caption=caption,
+                    )
+                elif len(post.images) > 1:
+                    media = [
+                        InputMediaPhoto(file, caption=caption if idx == 0 else "")
+                        for idx, file in enumerate(image_files)
+                    ]
+
+                    await _flood_wait(
+                        bot.send_media_group,
+                        chat_id=channel_id,
+                        media=media,
+                    )
+
+            if posts:
+                reddit_last_known_id = posts[-1].fullname
+                logger.info(f"Sent ~{len(posts)} posts, last seen id is {reddit_last_known_id!r}")
+            else:
+                logger.info("No new upvoted posts available")
+
+            await sleep(60)
 
 
 if __name__ == "__main__":
